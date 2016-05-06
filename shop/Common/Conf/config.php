b5ed4999262e5381338e7921837c13c05141349e
dingxiaohuan <wydingxiaohuan@163.com>
2016-05-06 20:41:21 +0800

    配置文件设置

--- shop/Common/Conf/config.php
+++ shop/Common/Conf/config.php
@@ -1,4 +1,11 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+	//伪静态配置
+	'URL_HTML_SUFFIX'           =>  'html',
+	//显示跟踪信息
+	'SHOW_PAGE_TRACE'           =>  true,
+	//默认分组名称
+	'DEFAULT_MODULE'            =>  'Home',
+	//定义可供访问的分组列表
+	'MODULE_ALLOW_LIST'         =>  array('Home','Admin')
 );
