f7b390c61c0402d7408e116fb229d3590c482fd3
dingxiaohuan <wydingxiaohuan@163.com>
2016-05-06 15:47:24 +0800

    搭建后台登录

---
+++ shop/Admin/Controller/ManagerController.class.php
@@ -0,0 +1,10 @@
+<?php
+//命名空间
+namespace Admin\Controller;
+use Think\Controller;
+class ManagerController extends Controller{
+	//管理员登录系统
+	function login(){
+		$this->display();
+	}
+}
--- shop/index.php
+++ shop/index.php
@@ -4,8 +4,12 @@
 define('APP_DEBUG',true);//开发(错误提示非常具体)
 //define('APP_DEBUG',false);//生产(错误提示模糊)
 //给静态资源文件访问目录设置常量,方便后期维护
+//Home分组
 define('CSS_URL','/mvc/shop/Public/css/');
 define('IMG_URL','/mvc/shop/Public/images/');
 define('JS_URL','/mvc/shop/Public/js/');
+//Admin分组
+define('ADMIN_CSS_URL','/mvc/shop/Admin/Public/css/');
+define('ADMIN_IMG_URL','/mvc/shop/Admin/Public/images/');
 //引入框架的接口文件
 include '../ThinkPHP/ThinkPHP.php';
