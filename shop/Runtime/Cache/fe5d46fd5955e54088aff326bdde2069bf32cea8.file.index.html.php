e32ed0b2b20577ef4fb01c19b2ac958bbf03bd97
dingxiaohuan <wydingxiaohuan@163.com>
2016-05-07 23:30:30 +0800

    mvc第二天

---
+++ shop/Runtime/Cache/fe5d46fd5955e54088aff326bdde2069bf32cea8.file.index.html.php
@@ -0,0 +1,43 @@
+<?php /* Smarty version Smarty-3.1.6, created on 2016-05-07 21:27:16
+         compiled from "E:/www/mvc/shop/Admin/View\Index\index.html" */ ?>
+<?php /*%%SmartyHeaderCode:22057572d63d99ee651-73983832%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'fe5d46fd5955e54088aff326bdde2069bf32cea8' => 
+    array (
+      0 => 'E:/www/mvc/shop/Admin/View\\Index\\index.html',
+      1 => 1462627634,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '22057572d63d99ee651-73983832',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.6',
+  'unifunc' => 'content_572d63d9a2516',
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_572d63d9a2516')) {function content_572d63d9a2516($_smarty_tpl) {?><!doctype html public "-//w3c//dtd xhtml 1.0 frameset//en" "http://www.w3.org/tr/xhtml1/dtd/xhtml1-frameset.dtd">
+<html>
+    <head>
+        <meta http-equiv=content-type content="text/html; charset=utf-8" />
+        <meta http-equiv=pragma content=no-cache />
+        <meta http-equiv=cache-control content=no-cache />
+        <meta http-equiv=expires content=-1000 />
+        <title>管理中心 v1.0</title>
+    </head>
+    <frameset border=0 framespacing=0 rows="60, *" frameborder=0>
+        <frame name=head src="<?php echo @__CONTROLLER__;?>
+/head" frameborder=0 noresize scrolling=no>
+            <frameset cols="170, *">
+                <frame name=left src="<?php echo @__CONTROLLER__;?>
+/left" frameborder=0 noresize />
+                <frame name=right src="<?php echo @__CONTROLLER__;?>
+/right" frameborder=0 noresize scrolling=yes />
+            </frameset>
+    </frameset>
+    <noframes>
+    </noframes>
+</html><?php }} ?>
